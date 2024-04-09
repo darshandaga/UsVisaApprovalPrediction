9206ce35481bde465f041cce4925a8dfa30055e9
darshandaga <61616902+darshandaga@users.noreply.github.com>
2024-04-09 08:26:21 +0000

    exception and logger added

--- demo.py
+++ demo.py
@@ -1,3 +1,10 @@
 from us_visa.logger import logging
+from us_visa.exception import USvisaException
 
 logging.info("welcom to custom logger")
+
+try :
+    a = 1 / "10"
+except Exception as e:
+    logging.info(e)
+    raise USvisaException(e, sys) from e
