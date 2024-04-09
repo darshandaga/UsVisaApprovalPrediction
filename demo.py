a89345ac3a64c680ff49ad555927e4395b29d230
darshandaga <61616902+darshandaga@users.noreply.github.com>
2024-04-09 18:34:42 +0000

    Updated

--- demo.py
+++ demo.py
@@ -1,10 +1,4 @@
-from us_visa.logger import logging
-from us_visa.exception import USvisaException
+from us_visa.constants import *
 
-logging.info("welcom to custom logger")
-
-try :
-    a = 1 / "10"
-except Exception as e:
-    logging.info(e)
-    raise USvisaException(e, sys) from e
+print(DATABASE_NAME)
+print(COLLECTION_NAME)
